ffa55c722d1d6834120e948fe9831315fa75702c
山村 達彦 <ruitomo.ex@gmail.com>
2014-06-08 02:53:54 +0900

    サンプルの追加

---
+++ Assets/NotificationObject/Sample/Sample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace notificationObjectSample
+{
+	public class Sample : MonoBehaviour
+	{
+		public static Sample Instance
+		{
+			get{
+				if( instance == null)
+					instance = GameObject.FindObjectOfType<Sample>();
+				return instance;
+			}
+		}
+		private static Sample instance = null;
+
+		public NotificationObject<int> item = new NotificationObject<int>(){ Value = 2 };
+
+		void OnGUI ()
+		{
+			if (GUILayout.Button ("add")) {
+				item.Value += 1;
+			}
+		}
+	}
+}
---
+++ Assets/NotificationObject/Sample/SampleGUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace notificationObjectSample
+{
+	public class SampleGUI : MonoBehaviour
+	{
+		public TextMesh textMesh ;
+
+		// Use this for initialization
+		void Start ()
+		{
+			var sample = Sample.Instance.item;
+			itemHandler(sample.Value);
+			sample.action += itemHandler;
+		}
+
+		void OnDestroy()
+		{
+			if( Sample.Instance != null)
+				Sample.Instance.item.action -= itemHandler;
+		}
+	
+		void itemHandler (int arg0)
+		{
+			textMesh.text = arg0.ToString("00000");
+		}
+	}
+}
