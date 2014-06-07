bf72704c0619bfb8cda5ca41f2cb4a280ef9029f
山村 達彦 <ruitomo.ex@gmail.com>
2014-06-08 02:53:36 +0900

    NotificationObjectの登録

---
+++ Assets/NotificationObject/NotificationObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NotificationObject<T> 
+{
+
+	private T data;
+	
+	public NotificationObject (T t)
+	{
+		Value = t;
+	}
+	
+	public NotificationObject ()
+	{}
+	
+	~NotificationObject ()
+	{ 
+		Dispose ();
+	}
+	
+	public UnityEngine.Events.UnityAction<T> action = null;
+
+	public T Value {
+		get {
+			return data;
+		}
+		set {
+			if( action != null )
+				action(value);
+			data = value;
+		}
+	}
+	
+	private void Notice ()
+	{
+		action (data);
+	}
+	
+	public void Dispose ()
+	{
+		action = null;
+	}
+}
